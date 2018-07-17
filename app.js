d414aa54149b3ede35454d9de2144aee5f8c2783
Tirumala Vasu Sykam <tirumalavasu4u@gmail.com>
2018-07-18 00:35:54 +0530

    drag and copy added will events

--- app.js
+++ app.js
@@ -1,54 +1,39 @@
-(function () {
-    "use strict"
-    let app = document.getElementById('app')
-    let layout = document.createElement('div')
-    layout.innerHTML = `        
-        <div class="container-fluid">
-	        <div class="row">
-		        <div class="col-md-6">
-		            <h1>Elements</h1>
-		            <div class="jumbotron" id="elements"></div>
-		        </div>
-		        <div class="col-md-6">
-		            <h1>Preview</h1>
-		            <div class="jumbotron" id="preview"></div>
-		        </div>
-		    </div>
-		</div>
-    `
-    app.appendChild(layout)
+function allowDrop (ev) {
+    ev.preventDefault()
+}
 
-    function allowDrop(ev) {
-        ev.preventDefault();
-    }
+function drag (ev) {
+    ev.dataTransfer.setData('text', ev.target.id)
+}
 
-    function drag(ev) {
-        ev.dataTransfer.setData("text", ev.target.id);
-    }
+function removeElement (e) {
+    document.getElementById(e.target.parentNode.parentNode.id).remove()
+}
 
-    function drop(ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
-    }
+function openConfig (e) {
+    // console.log(e.target.parentNode.parentNode.parentNode.id)
+    let eleId = e.target.parentNode.parentNode.parentNode.id.split('_')[0]
+    console.log(eleId)
+}
 
-    let elementsPanel = document.getElementById('elements')
-    let elements = document.createElement('div')
-    elements.setAttribute('draggable', 'true')
-    elements.addEventListener('ondragstart', drag)
-    elements.innerHTML = `
-        <div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">${Elements.input()}</div>
-    `
-    elementsPanel.appendChild(elements)
+function drop (ev) {
+    ev.preventDefault()
+    let data = ev.dataTransfer.getData('text')
+    /* If you use DOM manipulation functions, their default behaviour it not to
+       copy but to alter and move elements. By appending a ".cloneNode(true)",
+       you will not move the original element, but create a copy. */
+    let nodeCopy = document.getElementById(data).cloneNode(true)
+    nodeCopy.id = data + "_" + Math.floor(Math.random() * Math.floor(100))/* We cannot use the same ID */
 
-    let previewPanel = document.getElementById('preview')
-    let preview = document.createElement('div')
-    preview.setAttribute('draggable', 'true')
-    preview.addEventListener('ondrop', drop)
-    preview.addEventListener('ondragover', allowDrop)
-    preview.innerHTML = `
-        <div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-    `
-    previewPanel.appendChild(preview)
+    let configButton = document.createElement('div')
+    configButton.innerHTML = '<div class="config-button"><i class="fa fa-cog"></i></div>'
+    configButton.addEventListener('click', openConfig)
+    nodeCopy.prepend(configButton)
 
-})()
+    let removeButton = document.createElement('div')
+    removeButton.innerHTML = '<div class="remove-button">X</div>'
+    removeButton.addEventListener('click', removeElement)
+    nodeCopy.prepend(removeButton)
+
+    ev.target.appendChild(nodeCopy)
+}
