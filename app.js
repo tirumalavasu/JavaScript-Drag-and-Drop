385c96fa60ef7afd35b281b0d02b5074c974e818
Tirumala Vasu Sykam <tirumalavasu4u@gmail.com>
2018-07-19 11:12:02 +0530

    Rendering html elements by using data attributes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let mainPanel = document.getElementById('main')
+let configPanel = document.getElementById('configPanel')
+configPanel.style.visibility = 'hidden'
+
 function allowDrop (ev) {
     ev.preventDefault()
 }
@@ -14,6 +18,13 @@ function openConfig (e) {
     // console.log(e.target.parentNode.parentNode.parentNode.id)
     let eleId = e.target.parentNode.parentNode.parentNode.id.split('_')[0]
     console.log(eleId)
+    if (configPanel.style.visibility === 'hidden') {
+        configPanel.style.visibility = 'visible'
+        mainPanel.style.marginRight = '240px'
+    } else {
+        configPanel.style.visibility = 'hidden'
+        mainPanel.style.marginRight = '0'
+    }
 }
 
 function drop (ev) {
@@ -22,18 +33,30 @@ function drop (ev) {
     /* If you use DOM manipulation functions, their default behaviour it not to
        copy but to alter and move elements. By appending a ".cloneNode(true)",
        you will not move the original element, but create a copy. */
-    let nodeCopy = document.getElementById(data).cloneNode(true)
+    let draggedNode = document.getElementById(data).cloneNode(true)
+
+    let nodeCopy = document.createElement('div')
+
     nodeCopy.id = data + "_" + Math.floor(Math.random() * Math.floor(100))/* We cannot use the same ID */
+
+    let elementLabel = document.createElement('div')
+    elementLabel.innerHTML = draggedNode.getAttribute('data-label')
+    elementLabel.classList.add('element-label')
 
     let configButton = document.createElement('div')
     configButton.innerHTML = '<div class="config-button"><i class="fa fa-cog"></i></div>'
     configButton.addEventListener('click', openConfig)
-    nodeCopy.prepend(configButton)
 
     let removeButton = document.createElement('div')
     removeButton.innerHTML = '<div class="remove-button">X</div>'
     removeButton.addEventListener('click', removeElement)
+
+    // //Creating element using elements module
+    nodeCopy.innerHTML = Elements[draggedNode.getAttribute('data-type')]()
+
+    nodeCopy.prepend(configButton)
     nodeCopy.prepend(removeButton)
+    nodeCopy.prepend(elementLabel)
 
     ev.target.appendChild(nodeCopy)
 }
